eac9c1dc60c4fa11414fea2bc6e2920a94f23ca2
Nahuel Castilla <nahuelcastilla22@gmail.com>
2023-07-27 16:50:30 -0300

    portfolio final

--- src/Menu/Redes.jsx
+++ src/Menu/Redes.jsx
@@ -6,15 +6,15 @@ import instagram from '../assets/instagram.svg'
 const Redes = () => {
     return(
         <div className={styles.Redes}>
-           <a href="#">
+           <a href="https://linkedin.com/in/nahuel-castilla-dev">
             <img width='25px' src={Linkedin} alt="" />
            </a>
 
-           <a href="#">
+           <a href="https://github.com/nahuc22">
            <img width='25px' src={github} alt="" />
            </a>
 
-           <a href="#">
+           <a href="https://instagram.com/nahucastilla_">
            <img width='25px' src={instagram} alt="" />
            </a>
         </div>
